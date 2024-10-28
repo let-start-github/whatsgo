06c7c1b64d4edc94d312b88499328ad71fd5e4bf
let-start-github <119349656+let-start-github@users.noreply.github.com>
2024-10-28 22:28:17 +0530

    Update Signup.js
    third commit

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -100,10 +100,9 @@ const Signup = () => {
     if (pics.type === "image/jpeg" || pics.type === "image/png") {
       const data = new FormData();
       data.append("file", pics);
-      data.append("upload_preset", "aru-chat");
-      data.append("cloud_name", "dcrhrhwzo");
-      // cloudinary://544717474676148:MrF_CVAk37j13vG2zKxRcAfudow@dcrhrhwzo 
-      fetch("https://api.cloudinary.com/v1_1/dcrhrhwzo/image/upload", {
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "piyushproj");
+      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
         method: "post",
         body: data,
       })
